7c625461c2aa017e7bec05a7fbc782a3c6a35844
nolan_crrd <correardnolan@gmail.com>
2024-11-21 15:49:56 +0100

    last touch lien 2

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ function App() {
             <NavBar/>
             <Cursor/>
             <Routes>
-                <Route path=`${import.meta.env.BASE_URL} element={<Home/>}/>
-                <Route path=`${import.meta.env.BASE_URL}projects` element={<Projects/>}/>
-                <Route path=`${import.meta.env.BASE_URL}contact` element={<Contact/>}/>
+                <Route path={`${import.meta.env.BASE_URL}`} element={<Home/>}/>
+                <Route path={`${import.meta.env.BASE_URL}projects`} element={<Projects/>}/>
+                <Route path={`${import.meta.env.BASE_URL}contact`} element={<Contact/>}/>
             </Routes>
         </Router>
     );
