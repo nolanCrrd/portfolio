b0d6348565c4a02f0b9562b8098e9e0baffdf6d7
nolan_crrd <correardnolan@gmail.com>
2024-11-21 15:28:18 +0100

    bon lien dans router

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ function App() {
             <NavBar/>
             <Cursor/>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/projects" element={<Projects/>}/>
-                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/portfolio/" element={<Home/>}/>
+                <Route path="/portfolio/projects" element={<Projects/>}/>
+                <Route path="/portfolio/contact" element={<Contact/>}/>
             </Routes>
         </Router>
     );
