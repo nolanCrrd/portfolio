a88f171d62011115f1ad110f0d2c723160552cfd
nolan_crrd <correardnolan@gmail.com>
2024-11-21 15:18:05 +0100

    vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Nom du dépôt GitHub (pour GitHub Pages)
+  base: '/portfolio/', // Nom du dépôt GitHub (pour GitHub Pages)
   build: {
     outDir: 'dist', // Le dossier où le build est généré
     emptyOutDir: true, // Supprime les fichiers précédents dans 'dist' avant le build
