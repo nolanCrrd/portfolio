446464bf96a06b6c4c7f8c8faff47ea8e8cbb84c
Nolan Correard <correardnolan@gmail.com>
2024-12-12 10:13:36 +0100

    changement mrmv 2

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -124,7 +124,8 @@ function Projects() {
                 "This app allows you to create itineraries with a starting and an ending point, and find villages with heritage " +
                 "along your route where you can stop to rest. <br/><br/>" +
                 "You will also be able to specify what type of village or services you'd like to find. <br/><br/>" +
-                "Now, all you have to do is buckle up and embark on your adventure!</p>"
+                "Now, all you have to do is buckle up and embark on your adventure! <br/><br/>" + 
+                "This app is currently in developpement</p>"
 
         }
     };
